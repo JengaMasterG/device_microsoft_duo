442d8755694259847dde70cc996d8466b5d9259b
JengaMasterG <grsruggirello25@gmail.com>
2022-05-30 19:47:50 -0400

    Initial Commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/duo.mk
+
+COMMON_LUNCH_CHOICES := \
+    duo-user \
+    duo-userdebug \
+    duo-eng
