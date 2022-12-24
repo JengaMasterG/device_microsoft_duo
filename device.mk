9a9d5652d193d1aadeea3fef489573778474a132
JengaMasteG <grsruggirello25@gmail.com>
2022-12-24 12:15:46 -0500

    Remove libandroidicu: already defined

--- device.mk
+++ device.mk
@@ -43,9 +43,6 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl.recovery \
     bootctrl.$(PRODUCT_PLATFORM).recovery
 
-PRODUCT_COPY_FILES += \
-    $(OUT_DIR)/target/product/$(PRODUCT_RELEASE_NAME)/obj/SHARED_LIBRARIES/libandroidicu_intermediates/libandroidicu.so:$(TARGET_COPY_OUT_RECOVERY)/root/system/lib64/libandroidicu.so
-
 # HIDL
 PRODUCT_PACKAGES += \
     libhidltransport \
